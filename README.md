53c4f4b40ab75890300d72f144542be5fbb778f1
Kesus Kim <kesuskim@gmail.com>
2017-03-24 10:29:42 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple Electron Todo
+
+간단하게 Electron을 시작할 수 있는 Todo 샘플 어플리케이션입니다.
+
+_참조: [Vue TODO Sample App](https://jsfiddle.net/yyx990803/4dr2fLb7/?utm_source=website&utm_medium=embed&utm_campaign=4dr2fLb7)_
---
+++ main.js
@@ -0,0 +1,49 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const { dialog, ipcMain } = require('electron');
+
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 600, height: 600});
+
+  mainWindow.loadURL(url.format({
+    protocol: 'file:',
+    pathname: path.resolve(__dirname, 'index.html'),
+    slashes: true
+  }));
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+ipcMain.on('del-confirmation', (event, todo) => {
+  let isDelete = dialog.showMessageBox({
+    type: 'none',
+    buttons: ['취소', '삭제'],
+    title: '삭제 확인!',
+    message: `정말 항목 ${todo.title} 을 지울거에요??`
+  });
+  if (isDelete === 1) {
+    event.sender.send('del-confirmation', todo);
+  }
+});
